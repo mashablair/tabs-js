dd4f49e749ea09ff17c8b5ad013e843db960615c
Maria Blair <mashablair@gmail.com>
2020-01-03 14:02:22 -0800

    add JS

--- tabs.js
+++ tabs.js
@@ -1,1 +1,23 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector(".tabs");
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+function handleClick(event) {
+  if (!event.target.matches('[role="tab"]')) return;
+
+  // hide all tab panels
+  tabPanels.forEach(item => (item.hidden = true));
+
+  // mark all tabs as unselected
+  tabButtons.forEach(item => {
+    item.setAttribute("aria-selected", false);
+  });
+
+  // mark clicked tab as selected
+  event.target.setAttribute("aria-selected", true);
+
+  // find associated tabPanel and show it
+  tabs.querySelector(`[aria-labelledby="${event.target.id}"]`).hidden = false;
+}
+
+document.addEventListener("click", handleClick);
